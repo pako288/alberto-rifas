e775316bf386d9f6aa038df6bc18de8d3c376fdc
jose polanco <pakopolanco08@hotmail.com>
2024-12-26 22:47:51 -0400

    first Commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Banner from "./lib/Banner.svelte";
+  import Footer from "./lib/Footer.svelte";
+  import Header from "./lib/Header.svelte";
+
+  let fechaSorteo = `10 de Enero`
+	let premioMayor = `MOTO AVA TIGRITO 175CC 2024 IMPECABLE`
+	let rangoValue = $state(0);
+    let numeross = $state([]);
+	let nuevo = $state([]);
+    let numerosInitialValue = $state(`9999`);
+	let findNumber = $state([]);
+	let totalZelle = $state('');
+
+	let ticketValue = $state(20);
+    let formData = $state({ });
+	let numbersAvailable = $state([]);
+	let selectedTicket = $state([]);
+</script>
+
+<main>
+  <Header/>
+  <Banner {rangoValue} {ticketValue} {formData} {nuevo}  {fechaSorteo}, {premioMayor} ,{totalZelle}, {findNumber} 
+	{numbersAvailable}
+	{selectedTicket}
+	 />
+   <Footer/>
+  
+</main>
+
+
---
+++ src/lib/Header.svelte
@@ -0,0 +1,41 @@
+<script>
+    import logo from '../assets/images/logob.png';
+    // import menu from '../assets/images/menu.png';
+
+</script>
+
+<header>
+    <figure>
+        <img src={logo} alt="logo" height="90">
+    </figure>
+
+
+    <!-- <figure>
+        <img src={menu} alt="" width="40">
+    </figure> -->
+    <nav>
+        <a href="#">Inicio</a>
+        <a href="#buscador">Verificador</a>
+    </nav>
+</header>
+
+<style>
+    header{
+        padding: 10px 0;
+        border-bottom: 4px solid var(--terciary-red);
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 100px;
+    }
+
+    figure{
+        margin: 0;
+        padding: 0;
+    }
+
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+</style>
